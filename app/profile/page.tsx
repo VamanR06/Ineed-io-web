d75c1d6d33dd7845d2236bed9578cb8183ecec15
PraveshKunwar <praveshk04@outlook.com>
2025-02-09 01:53:04 -0500

    feat: just added dropdown menu for profile

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,15 +1,16 @@
 'use client';
 
 import '../globals.css';
-import React, { useEffect, useState } from 'react';
+import type React from 'react';
+import { useEffect, useState } from 'react';
 import { createClient } from '@/utils/supabase/client';
-import { Button } from '@/components/ui/button';
-import { User } from '@supabase/supabase-js';
-import { signOutAction } from '../actions';
+import type { User } from '@supabase/supabase-js';
+import { UserDropdownMenu } from '@/components/profile/user-dropdown-menu';
 
 const Profile: React.FC = () => {
   const [user, setUser] = useState<User | null>(null);
   const supabase = createClient();
+
   useEffect(() => {
     const fetchUser = async () => {
       const { data } = await supabase.auth.getUser();
@@ -20,16 +21,14 @@ const Profile: React.FC = () => {
   }, [supabase]);
 
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-gray-100">
-      <div className="w-full max-w-md rounded-lg bg-white p-8 shadow-md">
+    <div className="ineed.io-profile-page flex min-h-screen items-center justify-center">
+      <div className="mx-auto w-full max-w-md rounded-lg p-8 shadow-md">
         <h1 className="mb-4 text-center text-2xl font-bold">Profile Page</h1>
-        <p className="mb-6 text-center">Welcome {user?.email}</p>
-        <Button
-          onClick={signOutAction}
-          className="w-full rounded bg-red-500 px-4 py-2 font-bold text-white hover:bg-red-600"
-        >
-          Sign Out
-        </Button>
+        {user && (
+          <div className="flex justify-center">
+            <UserDropdownMenu user={user} />
+          </div>
+        )}
       </div>
     </div>
   );
