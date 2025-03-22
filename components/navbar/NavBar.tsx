18dc1aeebc1b144ec2cdbf7b6784e51aa59aed4c
Felix Shen <felixshn@umich.edu>
2025-03-22 11:22:34 -0400

    changed team route to aboutus route

--- components/navbar/NavBar.tsx
+++ components/navbar/NavBar.tsx
@@ -158,16 +158,6 @@ const NavBar = () => {
                 >
                   Settings
                 </Link>
-                <Link
-                  href="/team"
-                  className={`rounded-md px-3 py-2 text-sm font-medium ${
-                    pathname === '/team'
-                      ? 'bg-gray-700 text-white'
-                      : 'text-gray-300 hover:bg-gray-700 hover:text-white'
-                  }`}
-                >
-                  Team
-                </Link>
               </div>
             </div>
           </div>
