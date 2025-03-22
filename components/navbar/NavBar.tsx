a6d6d50d6fecde3071b920f7ac12b491a6f179fc
Felix Shen <felixshn@umich.edu>
2025-03-22 12:19:17 -0400

    navbar rework: refactored code + modifications

--- components/navbar/NavBar.tsx
+++ components/navbar/NavBar.tsx
@@ -9,6 +9,9 @@ import { UserDropdownMenu } from '../profile/user-dropdown-menu';
 import { createClient } from '@/utils/supabase/client';
 import { User } from '@/types/user';
 import { redirect } from 'next/navigation';
+import { Item } from '@radix-ui/react-dropdown-menu';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
 
 const NavBar = () => {
   const pathname = usePathname();
@@ -56,177 +59,77 @@ const NavBar = () => {
     fetchAvatar();
   }, [user]);
 
+  const links = [
+    { name: 'Home', route: '/' },
+    { name: 'Leaderboard', route: '/leaderboard' },
+    { name: 'About Us', route: '/aboutus' },
+    { name: 'Dashboard', route: '/dashboard' },
+    { name: 'Profile', route: '/profile' },
+  ];
+
   return (
-    <nav className="bg-black">
-      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-        <div className="relative flex h-16 items-center justify-between">
-          <div className="flex items-center">
-            <button
-              type="button"
-              className="focus:outline-hidden relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:ring-2 focus:ring-inset focus:ring-white sm:hidden"
-              aria-controls="mobile-menu"
-              aria-expanded={isMenuOpen ? 'true' : 'false'}
+    <nav className="relative flex h-20 items-center gap-2 bg-black p-4 sm:gap-8 sm:p-8">
+      <button
+        type="button"
+        className="focus:outline-hidden relative inline-flex items-center justify-center rounded-md border-white p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:ring-2 focus:ring-inset focus:ring-white sm:hidden"
+        aria-controls="mobile-menu"
+        aria-expanded={isMenuOpen ? 'true' : 'false'}
+        onClick={toggleMenu}
+      >
+        <span className="absolute -inset-0.5"></span>
+        <span className="sr-only">Open main menu</span>
+        <FontAwesomeIcon icon={faBars} className={`${isMenuOpen ? 'hidden' : 'block'} size-6`} />
+        <FontAwesomeIcon icon={faXmark} className={`${isMenuOpen ? 'block' : 'hidden'} size-6`} />
+      </button>
+      <div
+        className={`${isMenuOpen ? 'block' : 'hidden'} absolute left-[-2] top-20 z-10 w-[50%] sm:hidden`}
+        id="mobile-menu"
+      >
+        <div className="flex flex-col space-y-1 bg-primary px-2 pb-3 pt-2">
+          {links.map((item, idx) => (
+            <Link
+              key={`navbar-mobile-${idx}`}
+              href={item.route}
               onClick={toggleMenu}
+              className={`rounded-md px-3 py-2 text-sm font-medium ${
+                pathname === item.route
+                  ? 'bg-gray-700 text-white'
+                  : 'text-gray-300 hover:bg-gray-700 hover:text-white'
+              }`}
             >
-              <span className="absolute -inset-0.5"></span>
-              <span className="sr-only">Open main menu</span>
-              <svg
-                className={`${isMenuOpen ? 'hidden' : 'block'} size-6`}
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                aria-hidden="true"
-                data-slot="icon"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-                />
-              </svg>
-              <svg
-                className={`${isMenuOpen ? 'block' : 'hidden'} size-6`}
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                aria-hidden="true"
-                data-slot="icon"
-              >
-                <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
-              </svg>
-            </button>
-            <Link href="/" className="flex items-center">
-              <Image
-                className="h-8 w-auto"
-                src="/images/logo.png"
-                alt="Logo"
-                width={32}
-                height={32}
-              />
+              {item.name}
             </Link>
-            <div className="hidden sm:ml-6 sm:block">
-              <div className="flex space-x-4">
-                <Link
-                  href="/"
-                  className={`rounded-md px-3 py-2 text-sm font-medium ${
-                    pathname === '/'
-                      ? 'bg-gray-700 text-white'
-                      : 'text-gray-300 hover:bg-gray-700 hover:text-white'
-                  }`}
-                >
-                  Home
-                </Link>
-                <Link
-                  href="/dashboard"
-                  className={`rounded-md px-3 py-2 text-sm font-medium ${
-                    pathname === '/dashboard'
-                      ? 'bg-gray-700 text-white'
-                      : 'text-gray-300 hover:bg-gray-700 hover:text-white'
-                  }`}
-                >
-                  Dashboard
-                </Link>
-                <Link
-                  href="/profile"
-                  className={`rounded-md px-3 py-2 text-sm font-medium ${
-                    pathname === '/profile'
-                      ? 'bg-gray-700 text-white'
-                      : 'text-gray-300 hover:bg-gray-700 hover:text-white'
-                  }`}
-                >
-                  Profile
-                </Link>
-                <Link
-                  href="/leaderboard"
-                  className={`rounded-md px-3 py-2 text-sm font-medium ${
-                    pathname === '/leaderboard'
-                      ? 'bg-gray-700 text-white'
-                      : 'text-gray-300 hover:bg-gray-700 hover:text-white'
-                  }`}
-                >
-                  Leaderboard
-                </Link>
-                <Link
-                  href="/settings"
-                  className={`rounded-md px-3 py-2 text-sm font-medium ${
-                    pathname === '/settings'
-                      ? 'bg-gray-700 text-white'
-                      : 'text-gray-300 hover:bg-gray-700 hover:text-white'
-                  }`}
-                >
-                  Settings
-                </Link>
-              </div>
-            </div>
-          </div>
-          <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-            <ModeToggle />
-            {user ? <UserDropdownMenu avatar={avatarImage} /> : <></>}
-          </div>
+          ))}
         </div>
       </div>
-      <div className={`${isMenuOpen ? 'block' : 'hidden'} sm:hidden`} id="mobile-menu">
-        <div className="space-y-1 px-2 pb-3 pt-2">
+      <Link href="/" className="flex items-center gap-2 sm:gap-4">
+        <Image
+          className="size-8 w-auto border-white"
+          src="/images/logo.png"
+          alt="Logo"
+          width={32}
+          height={32}
+        />
+        <span className="font-semibol text-xl text-white">Ineed.io</span>
+      </Link>
+      <div className="hidden gap-8 sm:flex">
+        {links.map((item, idx) => (
           <Link
-            href="/"
-            className="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white"
-            aria-current="page"
-          >
-            Home
-          </Link>
-          <Link
-            href="/dashboard"
+            href={item.route}
+            key={`navbar-${idx}`}
             className={`rounded-md px-3 py-2 text-sm font-medium ${
-              pathname === '/dashboard'
+              pathname === item.route
                 ? 'bg-gray-700 text-white'
                 : 'text-gray-300 hover:bg-gray-700 hover:text-white'
             }`}
           >
-            Dashboard
+            {item.name}
           </Link>
-          <Link
-            href="/profile"
-            className={`rounded-md px-3 py-2 text-sm font-medium ${
-              pathname === '/profile'
-                ? 'bg-gray-700 text-white'
-                : 'text-gray-300 hover:bg-gray-700 hover:text-white'
-            }`}
-          >
-            Profile
-          </Link>
-          <Link
-            href="/settings"
-            className={`rounded-md px-3 py-2 text-sm font-medium ${
-              pathname === '/settings'
-                ? 'bg-gray-700 text-white'
-                : 'text-gray-300 hover:bg-gray-700 hover:text-white'
-            }`}
-          >
-            Leaderboard
-          </Link>
-          <Link
-            href="/leaderboard"
-            className={`rounded-md px-3 py-2 text-sm font-medium ${
-              pathname === '/leaderboard'
-                ? 'bg-gray-700 text-white'
-                : 'text-gray-300 hover:bg-gray-700 hover:text-white'
-            }`}
-          >
-            Settings
-          </Link>
-          <Link
-            href="/team"
-            className={`rounded-md px-3 py-2 text-sm font-medium ${
-              pathname === '/team'
-                ? 'bg-gray-700 text-white'
-                : 'text-gray-300 hover:bg-gray-700 hover:text-white'
-            }`}
-          >
-            FAQ
-          </Link>
-        </div>
+        ))}
+      </div>
+      <div className="ml-auto flex items-center gap-2 sm:gap-4">
+        <ModeToggle />
+        {user ? <UserDropdownMenu avatar={avatarImage} /> : <></>}
       </div>
     </nav>
   );
