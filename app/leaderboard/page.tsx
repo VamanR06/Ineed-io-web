028cf6c84fdaf4a551ac18fc9183b6d62877d20a
PraveshKunwar <praveshk04@outlook.com>
2025-03-26 18:23:06 -0400

    feat: adding framer motion, and updating todos

--- app/leaderboard/page.tsx
+++ app/leaderboard/page.tsx
@@ -1,18 +1,30 @@
+'use client';
+
 import '../globals.css';
 import React, { JSX } from 'react';
 import { LeaderboardHeader } from '@/components/leaderboard/header';
 import { LeaderboardMetrics } from '@/components/leaderboard/metrics';
 import { LeaderboardTable } from '@/components/leaderboard/table';
+import { motion } from 'framer-motion';
+
+const fadeInVariants = { initial: { opacity: 0 }, animate: { opacity: 1 } };
 
 const Explore: React.FC = (): JSX.Element => {
   return (
-    <div className="ineed.io-explore-page min-h-screen p-6 text-white">
-      <LeaderboardHeader />
-      <div className="mt-6 space-y-6">
-        <LeaderboardMetrics />
-        <LeaderboardTable />
+    <motion.div
+      initial="initial"
+      animate="animate"
+      variants={fadeInVariants}
+      transition={{ duration: 1, ease: 'easeInOut' }}
+    >
+      <div className="ineed.io-explore-page min-h-screen p-6 text-white">
+        <LeaderboardHeader />
+        <div className="mt-6 space-y-6">
+          <LeaderboardMetrics />
+          <LeaderboardTable />
+        </div>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
