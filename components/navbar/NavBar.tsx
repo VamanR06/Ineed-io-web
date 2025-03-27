b3f581009a1e812922ef4548c8cab801f5f68c16
Felix Shen <felixshn@umich.edu>
2025-03-27 14:56:11 -0400

    fixed: home link in navbar now gets highlighted when on dashboard route

--- components/navbar/NavBar.tsx
+++ components/navbar/NavBar.tsx
@@ -79,7 +79,7 @@ const NavBar = () => {
               href={item.route}
               onClick={toggleMenu}
               className={`rounded-md px-3 py-2 text-sm font-medium ${
-                pathname === item.route
+                (pathname === '/dashboard' && item.name === 'Home' ? true : pathname === item.route)
                   ? 'bg-gray-700 text-white'
                   : 'text-gray-300 hover:bg-gray-700 hover:text-white'
               }`}
@@ -105,7 +105,7 @@ const NavBar = () => {
             href={item.route}
             key={`navbar-${idx}`}
             className={`rounded-md px-3 py-2 text-sm font-medium ${
-              pathname === item.route
+              (pathname === '/dashboard' && item.name === 'Home' ? true : pathname === item.route)
                 ? 'bg-gray-700 text-white'
                 : 'text-gray-300 hover:bg-gray-700 hover:text-white'
             }`}
